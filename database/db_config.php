350d9072f4d35badf2ba8c323cb8fd2c49e2e5c7
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-04-11 15:12:14 +0530

    updated database connection

--- database/db_config.php
+++ database/db_config.php
@@ -2,16 +2,16 @@
 // database/db_config.php
 
 $host = 'localhost';
-$db   = 'bhharatutthann_db';
-$user = 'root';
-$pass = '';
+$db = 'bargain1_bharat';
+$user = 'bargain1_bharat';
+$pass = 'Bharat@2026';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 try {
